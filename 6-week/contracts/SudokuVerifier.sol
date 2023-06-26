f3b7ceb2422a26903f21ac747e47f88feb619623
Meek Msaki <meek10x@gmail.com>
2023-06-26 02:47:22 -0500

    chore: init woke sudoku

---
+++ 6-week/contracts/SudokuVerifier.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract SudokuVerifier {
+    function checkSudoku(
+        uint8[9][9] calldata solution
+    ) external pure returns (bool) {}
+}
